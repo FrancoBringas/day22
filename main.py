1f0bde452858ecd12c39141d6072c5945449107b
FrancoBringas <francobringas17@gmail.com>
2022-02-11 12:30:01 -0300

    main.py paddle.py ball.p

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
-from turtle import Screen,Turtle
-from paddle import Paddle
+from turtle import Screen
+from padle import Paddle
+from ball import Ball
+import time
 MOVE_DISTANCE = 20
-X=350
-Y=0
+X = 350
+Y = 0
 game = True
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("pong game")
-# screen.tracer(0)
-
-
+screen.tracer(0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
 screen.listen()
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(X, new_y)
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(X, new_y)
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
-
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 while game:
     screen.update()
+    ball.move()
+    time.sleep(0.1)
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        screen.update()
+        ball.bounce()
 screen.exitonclick()
